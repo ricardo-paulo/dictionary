510b45226c972627e5fbf6b0eb2e4a9815daf0b8
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2026-04-15 16:23:15 -0300

    Métodos para verificação de desbalanceamento
    Foram implementados os métodos:
    calcHeight e calcHeightRecursively para cálculo da altura em relação a um determinado termo (nó); e
    
    checkBalancing para verificar se há desbalanceamento, se houver irá retornar o lado (desbTermSide) juntamente com um booleano isBalanced.

--- src/AVL.js
+++ src/AVL.js
@@ -1,4 +1,3 @@
-import checkBalancing from "./functions/checkBalancing.js"
 import fillTermsTree from "./functions/fillTermsTree.js"
 
 class Term {
@@ -25,6 +24,51 @@ class AVL {
         fillTermsTree(this)
     }
 
+    static calcHeight (current) {
+        const { counter, max } = this.#calcHeightRecursively(current, 0, 0)
+        
+        return max
+
+    }
+    
+    static #calcHeightRecursively (current, counter, max) {
+        if (current == null) {
+            return {counter, max}
+        }
+        
+        counter++
+        if (counter > max) 
+            max = counter
+        
+        const result1 = this.#calcHeightRecursively(current.left, counter, max)
+        const result2 = this.#calcHeightRecursively(current.right, counter, max)
+        const resultsMajor = result1.max > result2.max ? result1.max : result2.max
+        max = max < resultsMajor ? resultsMajor : max
+        counter--
+        
+        return {counter, max}
+
+    }
+
+    static checkBalancing (current) {
+        const leftHeight = this.calcHeight(current.left)
+        const rightHeight = this.calcHeight(current.right)
+        const heightDiff = leftHeight - rightHeight
+        const isBalanced = heightDiff >= -1 && heightDiff <= 1
+        let desbTermSide = null
+
+        if (!isBalanced) {
+            if (leftHeight > rightHeight) {
+                desbTermSide = 'left'
+            } else {
+                desbTermSide = 'right'
+            }
+        }
+
+        return {isBalanced, desbTermSide}
+
+    }
+
     addTerm (newTerm) {
         if (!newTerm instanceof Term) {
             console.log(`O objeto: ${newTerm}\nNão é um objeto de Term válido.`)
@@ -46,7 +90,7 @@ class AVL {
             if (current.left === null) {
                 current.left = newTerm
                 newTerm.parent = current
-                checkBalancing(newTerm, current, newTermToLeft)
+                // AVL.checkBalancing(current)
             } else {
                 this.#addTermRecursively(newTerm, current.left)
             }
@@ -54,28 +98,28 @@ class AVL {
             if (current.right === null) {
                 current.right = newTerm
                 newTerm.parent = current
-                checkBalancing(newTerm, current, newTermToLeft)
+                // AVL.checkBalancing(newTerm, current, newTermToLeft)
             } else {
                 this.#addTermRecursively(newTerm, current.right)
             }
         }
     }
 
-    determinateSide (target) {
+    targetOnParentSide (target) {
         
         if (target.parent.left == target) {
             return 'left'
         } else if (target.parent.right == target) {
             return 'right'
         } else {
-            return 'no-found'
+            return 'notFound'
         }
 
     }
 
     rotateTerm (anchorTerm, toLeft = true) {
         
-        const anchorRotationDirection = this.determinateSide(anchorTerm)
+        const anchorRotationDirection = this.targetOnParentSide(anchorTerm)
         const childRotationDirection = anchorRotationDirection == 'left' ? 'right' : 'left'
 
         if (toLeft) {
