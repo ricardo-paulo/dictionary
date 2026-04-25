3e4aa4f7343cafc9d18e82eb0c4ce7e958a0963b
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2026-04-25 12:07:22 -0300

    Modificações de interface
    Foram mudados alguns detalhes da interface, como correção de palavras.
    
    Foi código HTML e CSS foram refatorados para utilizarem Bootstrap.
    
    E foi criado o componente personalizado DefinitionCard, que encapsula o cartão de cada definição (Term) que será adicionado a lista de definições.

---
+++ custom/DefinitionCard.js
@@ -0,0 +1,66 @@
+class DefinitionCard extends HTMLElement {
+
+    constructor () {
+        super()
+    }
+
+    connectedCallback () {
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'word-card')
+        
+        const cardHeader = container.appendChild(document.createElement('div'))
+        cardHeader.setAttribute('class', 'd-flex justify-content-between align-items-center mb-2')
+
+        const cardTitle = cardHeader.appendChild(document.createElement('p'))
+        cardTitle.setAttribute('class', 'card-title')
+        cardTitle.innerText = this.hasAttribute('card-title')
+        ? this.getAttribute('card-title') 
+        : 'Título não encontrado'
+
+        const dropdown = cardHeader.appendChild(document.createElement('div'))
+        dropdown.setAttribute('class', 'dropdown')
+        
+        const optionsButton = dropdown.appendChild(document.createElement('button'))
+        optionsButton.setAttribute('class', 'btn-opcoes dropdown-toggle')
+        optionsButton.setAttribute('type', 'button')
+        optionsButton.setAttribute('data-bs-toggle', 'dropdown')
+        optionsButton.setAttribute('aria-expanded', 'false')
+        optionsButton.innerText = 'OPÇÕES'
+
+        const dropdownMenu = dropdown.appendChild(document.createElement('ul'))
+        dropdownMenu.setAttribute('class', 'dropdown-menu dropdown-menu-end')
+
+        const editButton_li = dropdownMenu.appendChild(document.createElement('li'))
+
+        const editButton = editButton_li.appendChild(document.createElement('button'))
+        editButton.setAttribute('class', 'dropdown-item edit-button')
+        editButton.innerText = '✏️ Editar'
+
+        const deleteButton_li = dropdownMenu.appendChild(document.createElement('li'))
+
+        const deleteButton = deleteButton_li.appendChild(document.createElement('button'))
+        deleteButton.setAttribute('class', 'dropdown-item text-danger delete-button')
+        deleteButton.innerText = '🗑️ Excluir'
+
+        const cardBody = container.appendChild(document.createElement('div'))
+        cardBody.setAttribute('class', 'card-body-content ps-2')
+
+        const subWord = cardBody.appendChild(document.createElement('div'))
+        subWord.setAttribute('class', 'sub-word mb-1')
+        subWord.innerText = this.hasAttribute('card-sub-word')
+        ? this.getAttribute('card-sub-word')
+        : 'Sub-palavra não encontrada'
+        
+        const description = cardBody.appendChild(document.createElement('p'))
+        description.setAttribute('class', 'description')
+        description.innerText = this.hasAttribute('card-description')
+        ? this.getAttribute('card-description')
+        : 'Descrição não encontrada'
+        
+        this.append(container)
+
+    }
+}
+
+customElements.define('definition-card', DefinitionCard)
