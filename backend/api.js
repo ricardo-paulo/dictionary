dfc642edbc0f699f776be8dd70b73ef1eb652f3e
Paulo Ricardo <paulo.silva56@estudante.ifto.edu.br>
2026-04-28 08:23:49 -0300

    Correção de estrutura
    Foi realizada uma correção na localização dos arquivos para que os arquivos da API ficassem contidos na pasta backend.

---
+++ backend/api.js
@@ -0,0 +1,93 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import cors from 'cors'
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+app.use(express.json());
+
+const terms = path.resolve("data/terms.json");
+
+app.get("/terms", (req, res) => {
+  const dados = fs.readFileSync(terms, "utf-8");
+  res.json(JSON.parse(dados));
+});
+
+app.get("/terms/:palavra", (req, res) => {
+  const dados = fs.readFileSync(terms, "utf-8");
+  const arquivo = JSON.parse(dados);
+
+  const busca = req.params.palavra.toLowerCase(); 
+  const resultado = arquivo.find(item => item.portuguese.toLowerCase() === busca ); 
+  
+  if (resultado) { 
+    res.json(resultado); 
+  } else { 
+    res.json({ mensagem: "Termo não encontrado" });
+}});
+
+app.post("/terms", (req, res) => {
+  const dados = fs.readFileSync(terms, "utf-8");
+  const arquivo = JSON.parse(dados);
+
+  const nova = req.body;
+  arquivo.push(nova);
+
+  fs.writeFileSync(terms, JSON.stringify(arquivo, null, 2));
+  res.json({ mensagem: "Palavra adicionada ", palavra: nova});
+});
+
+app.put("/terms/:palavra", (req, res) => {
+  const dados = fs.readFileSync(terms, "utf-8");
+  const arquivo = JSON.parse(dados);
+
+  const busca = req.params.palavra.toLowerCase();
+
+  const novaLista = arquivo.map(item => {
+
+    if (
+      item &&
+      item.portuguese &&
+      item.portuguese.toLowerCase() === busca
+    ) {
+      return req.body;
+    }
+
+    return item;
+  });
+
+  fs.writeFileSync(terms, JSON.stringify(novaLista, null, 2));
+
+  res.json({
+    mensagem: "dado atualizado",
+    dados: req.body
+  });
+});
+
+app.delete("/terms/:palavra", (req, res) => {
+  const dados = fs.readFileSync(terms, "utf-8");
+  let arquivo = JSON.parse(dados);
+
+  const busca = req.params.palavra.toLowerCase().trim();
+
+  const tamanhoAntes = arquivo.length;
+
+  arquivo = arquivo.filter(item =>
+    item.portuguese.toLowerCase().trim() !== busca
+  );
+
+  if (arquivo.length === tamanhoAntes) {
+    return res.json({ mensagem: "Palavra não encontrada." });
+  }
+
+  fs.writeFileSync(terms, JSON.stringify(arquivo, null, 2));
+
+  res.json({ mensagem: "Palavra removida." });
+});
+
+app.listen(PORT, () => {
+  console.log(`O servidor está rodando na porta ${PORT}`);
+});
